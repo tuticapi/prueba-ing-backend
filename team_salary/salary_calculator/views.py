9dd819c0d9e5edd19a0180711de87cf3056eef75
Diego Magaldi <realiversity@hotmail.com>
2022-01-06 13:57:22 -0600

    testing

--- team_salary/salary_calculator/views.py
+++ team_salary/salary_calculator/views.py
@@ -15,13 +15,12 @@ class CalculatorView(APIView):
         data = request.data
         if('jugadores' not in data):
             return Response({"error": "missing information"},
+                            content_type="application/json",
                             status=status.HTTP_400_BAD_REQUEST)
-
         serializer = MemberSerializer(data=data.get('jugadores'), many=True)
         serializer.is_valid(raise_exception=True)
         core = Core(serializer.data)
         team_salaries = core.calculate_salary()
-
         team = MemberSalarySerializer(data=team_salaries, many=True)
         team.is_valid(raise_exception=True)
         return Response(team.data,
